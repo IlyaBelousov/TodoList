0753584ab4c61d86f223c089dcc9e82bd37ad64d
IlyaBelousov <77740267+IlyaBelousov@users.noreply.github.com>
2021-07-17 21:49:17 +0300

    Updates

---
+++ precache-manifest.a209f3239643d6d12b771b30c79257e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258c996bc6f0d1d31393114d6815204a",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "6bad3c489000dd03cccb",
+    "url": "/TodoList/static/css/main.146db43e.chunk.css"
+  },
+  {
+    "revision": "c6587eb71ded6dca277f",
+    "url": "/TodoList/static/js/2.4a4e4460.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/TodoList/static/js/2.4a4e4460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bad3c489000dd03cccb",
+    "url": "/TodoList/static/js/main.9cadce97.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
