0fb3737656451a37f8ff46c81d44747d8f642331
IlyaBelousov <77740267+IlyaBelousov@users.noreply.github.com>
2021-08-01 16:23:28 +0300

    Updates

--- precache-manifest.4a729be3a0fd08880ca1a342b4f7ef06.js
+++ precache-manifest.4a729be3a0fd08880ca1a342b4f7ef06.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "258c996bc6f0d1d31393114d6815204a",
+    "revision": "7adf9898c7d6a074c7848998f454d6d1",
     "url": "/TodoList/index.html"
   },
   {
-    "revision": "6bad3c489000dd03cccb",
+    "revision": "042db0f701b62fcccaa5",
     "url": "/TodoList/static/css/main.146db43e.chunk.css"
   },
   {
-    "revision": "c6587eb71ded6dca277f",
-    "url": "/TodoList/static/js/2.4a4e4460.chunk.js"
+    "revision": "4ca3d06a3555268be966",
+    "url": "/TodoList/static/js/2.7417dfee.chunk.js"
   },
   {
     "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
-    "url": "/TodoList/static/js/2.4a4e4460.chunk.js.LICENSE.txt"
+    "url": "/TodoList/static/js/2.7417dfee.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6bad3c489000dd03cccb",
-    "url": "/TodoList/static/js/main.9cadce97.chunk.js"
+    "revision": "042db0f701b62fcccaa5",
+    "url": "/TodoList/static/js/main.29e637b3.chunk.js"
   },
   {
     "revision": "6f4002f704c746794b97",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TodoList/precache-manifest.a209f3239643d6d12b771b30c79257e6.js"
+  "/TodoList/precache-manifest.4a729be3a0fd08880ca1a342b4f7ef06.js"
 );
 
 self.addEventListener('message', (event) => {
